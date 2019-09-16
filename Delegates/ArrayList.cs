6627dfb6329fd2dcbb5e32f218b02eace146403e
Ryan LaCombe <ryanalacombe@gmail.com>
2019-09-16 12:26:32 -0500

    Add project files.

---
+++ Delegates/ArrayList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class ArrayList
+    {
+        public virtual int Add(object value)
+        {
+            int i = 0;
+            ArrayList[] continuedArray = new ArrayList[i];
+            i = (continuedArray.Length + 1);
+            for (int o = 0; o < continuedArray.Length; o++)
+            {
+                continuedArray[o] = continuedArray[o];
+                int arrayEnd = (continuedArray.Length - 1);
+                value = continuedArray[arrayEnd];
+                i++;
+            }
+
+            return i;
+        }
+        public virtual void Clear()
+        {
+
+        }
+        public virtual object this[int index]
+        {
+            set
+            {
+                
+            }
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new list
+            ListWithChangedEvent list = new ListWithChangedEvent();
+
+            //Create a class that listens for when the list is changed
+            EventListener listener = new EventListener(list);
+
+            list.Add("hello");
+            list.Add("world");
+            list.Clear();
+            listener.Detatch();
+
+            Console.ReadKey();
+        }
+    }
+}
